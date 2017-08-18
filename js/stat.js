216e7f45e9aba8db373f4d75e572b18a9c70269b
StanislauSk <skopenko@tut.by>
2017-08-18 14:07:32 +0300

    Изменил файл stat

--- js/stat.js
+++ js/stat.js
@@ -1,37 +1,55 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  printRect(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+  printRect(ctx, 100, 10, 420, 270, 'white');
+  printText(ctx, 'Ура вы победили!', 120, 40, '16px PT Mono', '#000');
+  printText(ctx, 'Список результатов:', 120, 60, '16px PT Mono', '#000');
+  printHistogram(ctx, names, times);
+  function printRect(canvas, x, y, width, height, color) {
+    canvas.fillStyle = color || 'white';
+    canvas.fillRect(x, y, width, height);
+  }
+  function printText(canvas, text, x, y, font, style) {
+    canvas.fillStyle = style || 'black';
+    canvas.font = font || '16px PT Mono';
+    canvas.fillText(text, x, y);
+  }
+  function printHistogram(canvas, namesArr, timesArr) {
+    var max = -1;
+    var histogramHeight = 150;
+    var i = 0;
+    var j = 0;
+    var distance = 140;
 
-  var max = -1;
-
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
+    for (i; i < timesArr.length; i++) {
+      var maxTime = timesArr[i];
+      if (maxTime > max) {
+        max = maxTime;
+      }
+    }
+    var step = histogramHeight / max;
+    for (j; j < timesArr.length; j++) {
+      histogramHeight = timesArr[j] * step;
+      canvas.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
+      printText(canvas, namesArr[j], distance, 260, '16px PT Mono', '#000');
+      printText(canvas, parseInt(timesArr[j], 10), distance, 230 - histogramHeight);
+      printRect(canvas, distance, 250, 40, -histogramHeight, getRandomColor(namesArr[j]));
+      distance += 90;
     }
   }
 
-  var histogramheight = 150;
-  var step = histogramheight / (max - 0);
-
-  ctx.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
-  for (i = 0; i < times.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], 140 + 90 * i, 250);
-    ctx.fillText(parseInt(times[i], 10), 140 + 90 * i, 240 - times[i] * step - 30);
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  function getRandomColor(name) {
+    var result = null;
+    var random = Math.round(Math.random() * 10) / 10;
+    if (name === 'Вы') {
+      result = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.round(Math.random() * 10) / 10 + ')';
+      while (random < 0.2) {
+        random = Math.round(Math.random() * 10) / 10;
+      }
+      result = 'rgba(0, 0, 255, ' + random + ')';
     }
-    ctx.fillRect(140 + 90 * i, 240, 40, -times[i] * step);
+    return result;
   }
 };
